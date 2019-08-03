1e1e156436693c9ce72bd7956e4f41924d0a32d4
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-03 18:11:37 -0400

    added public directory

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('view options', { delimiter: '?' });
-// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // root route
 app.get('/', (req, res) => {
