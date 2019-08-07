e1f283feff3527af23a21425021ef22d6a8de191
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-07 14:24:58 -0400

    required mongoose and connected to test db

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/spot_for_rocks', {
+  useNewUrlParser: true
+});
 
 app.set('view engine', 'ejs');
 app.set('view options', { delimiter: '?' });
