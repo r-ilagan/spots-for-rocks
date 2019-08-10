de35e9d03fead9a4bf0288bfc00df7db8bbc8602
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-10 01:34:01 -0400

    commented out db related stuff for now

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/spot_for_rocks', {
-  useNewUrlParser: true
-});
+// mongoose.connect('mongodb://localhost:27017/spot_for_rocks', {
+//   useNewUrlParser: true
+// });
 
 app.set('view engine', 'ejs');
 app.set('view options', { delimiter: '?' });
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 // root route
 app.get('/', (req, res) => {
