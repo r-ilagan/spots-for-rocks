33bce09845541de46d09f0a92ed547af04dec6b7
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-10 11:32:09 -0400

    added port with a config var

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ app.set('view options', { delimiter: '?' });
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
+const port = process.env.PORT || 3000;
+
 // root route
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server has started and listening on port 3000!');
 });
