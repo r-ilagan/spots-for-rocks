abe4ad9d37be50b433d344810d7d9c5890e1bd26
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-13 22:29:50 -0400

    removed comments attribute of spots

--- models/spots.js
+++ models/spots.js
@@ -4,8 +4,8 @@ const spotSchema = new mongoose.Schema({
   name: String,
   description: String,
   author: String,
-  image: String,
-  comment: [String]
+  image: String
+  // comment: [String]
 });
 
 module.exports = mongoose.model('Spots', spotSchema);
