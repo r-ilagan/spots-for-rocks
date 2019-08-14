4fc2bab1ff6aaa5282b2e20955b29b0a28fd1e16
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-13 22:33:33 -0400

    added create route for new spots

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+/* eslint-disable prefer-destructuring */
 const express = require('express');
 const app = express();
 const path = require('path');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Spots = require('./models/spots');
 
-// mongoose.connect('mongodb://localhost:27017/spot_for_rocks', {
-//   useNewUrlParser: true
-// });
+mongoose.connect('mongodb://localhost:27017/spot_for_rocks', {
+  useNewUrlParser: true
+});
 
 app.set('view engine', 'ejs');
 app.set('view options', { delimiter: '?' });
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const port = process.env.PORT || 3000;
 
 // root route
@@ -21,7 +23,7 @@ app.get('/', (req, res) => {
   res.render('landing');
 });
 
-// Show route
+// Index route
 app.get('/spots', (req, res) => {
   res.render('index/spots');
 });
@@ -32,8 +34,24 @@ app.get('/spots/new', (req, res) => {
 });
 
 // Create route
-app.post('/spots', (req, res) => {
-  res.render('index/spots');
+app.post('/spots', urlencodedParser, (req, res) => {
+  const author = req.params.author;
+  const image = req.params.image;
+  const place = req.params.place;
+  const description = req.params.description;
+  const newSpot = {
+    place,
+    description,
+    author,
+    image
+  };
+  Spots.create({ newSpot }, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index/spots');
+    }
+  });
 });
 
 app.listen(port, () => {
