6581966062ab8a6272167982789a615c6b1d8496
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-18 22:40:40 -0400

    placed express.urlencoded above routes to make it useable

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/* eslint-disable object-shorthand */
-/* eslint-disable prefer-destructuring */
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -8,10 +6,10 @@ const spotRoutes = require('./routes/spots');
 const app = express();
 require('dotenv').config();
 
-// const testDB = 'mongodb://localhost:27017/spot_for_rocks';
+const testDB = 'mongodb://localhost:27017/spot_for_rocks';
 const realDB = process.env.DB_URL;
 mongoose
-  .connect(`${realDB}`, {
+  .connect(`${testDB}`, {
     useNewUrlParser: true
   })
   .then(() => {
@@ -25,13 +23,13 @@ app.set('view engine', 'ejs');
 app.set('view options', { delimiter: '?' });
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Routes
 app.use(indexRoutes);
-app.use(spotRoutes);
+app.use('/spots', spotRoutes);
 
-app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
