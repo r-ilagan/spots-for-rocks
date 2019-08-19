6962c240d91a95fb8e3aa6e677c6c8b1d3cff53e
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-18 20:09:32 -0400

    moved out the index routes and other spot routes into their own files

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 /* eslint-disable object-shorthand */
 /* eslint-disable prefer-destructuring */
 const express = require('express');
-const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const Spots = require('./models/spots');
+const indexRoutes = require('./routes/index');
+const spotRoutes = require('./routes/spots');
+const app = express();
 require('dotenv').config();
 
 // const testDB = 'mongodb://localhost:27017/spot_for_rocks';
@@ -26,50 +27,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(express.json());
 
-const urlencodedParser = express.urlencoded({ extended: false });
-const port = process.env.PORT || 5000;
+// Routes
+app.use(indexRoutes);
+app.use(spotRoutes);
 
-// root route
-app.get('/', (req, res) => {
-  res.render('landing');
-});
-
-// Index route
-app.get('/spots', (req, res) => {
-  Spots.find({}, (err, foundSpots) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('index/spots', { spots: foundSpots });
-    }
-  });
-});
-
-// New route
-app.get('/spots/new', (req, res) => {
-  res.render('index/new');
-});
-
-// Create route
-app.post('/spots', urlencodedParser, (req, res) => {
-  const author = req.body.author;
-  const image = req.body.image;
-  const place = req.body.place;
-  const description = req.body.description;
-  const newSpot = {
-    name: place,
-    description: description,
-    author: author,
-    image: image
-  };
-  Spots.create(newSpot, err => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect('/spots');
-    }
-  });
-});
+app.use(express.urlencoded({ extended: false }));
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log('Server has started!');
