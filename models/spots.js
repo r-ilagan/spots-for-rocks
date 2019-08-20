c8a3acaff98df594636219fc1f6a7ac8b7cdc573
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-20 15:53:13 -0400

    Added timestamps to spots model to show when a Spot is created and updated

--- models/spots.js
+++ models/spots.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
-const spotSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  author: String,
-  image: String
-  // comment: [String]
-});
+const spotSchema = new mongoose.Schema(
+  {
+    name: String,
+    description: String,
+    author: String,
+    image: String
+    // created: { type: Date, default: Date.now }
+    // comment: [String]
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Spots', spotSchema);
