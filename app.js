c2612d0ee4b9d457358b99cf2c54eeb5b37b373e
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-20 20:58:28 -0400

    Required and set method-override middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const indexRoutes = require('./routes/index');
 const spotRoutes = require('./routes/spots');
 const app = express();
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 // Routes
 app.use(indexRoutes);
