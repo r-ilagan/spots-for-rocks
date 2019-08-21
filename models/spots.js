7ee922733053e35331eb8834da9fdca07977c25a
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-20 22:24:12 -0400

    Added wasUpdated attribute to Spot schema for checking if a spot was updated

--- models/spots.js
+++ models/spots.js
@@ -5,8 +5,8 @@ const spotSchema = new mongoose.Schema(
     name: String,
     description: String,
     author: String,
-    image: String
-    // created: { type: Date, default: Date.now }
+    image: String,
+    wasUpdated: Boolean
     // comment: [String]
   },
   { timestamps: true }
