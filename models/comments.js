c953ef725e77fccfa5b046464e6a69c27ce39fd9
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-22 15:07:27 -0400

    Added timestamps

--- models/comments.js
+++ models/comments.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-const commentSchema = new mongoose.Schema({
-  author: String,
-  text: String
-});
+const commentSchema = new mongoose.Schema(
+  {
+    author: String,
+    text: String
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Comment', commentSchema);
