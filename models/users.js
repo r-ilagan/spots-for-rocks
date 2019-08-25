29193fbd9c2873d32cc1ddfa9ac98afcd0f9c899
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-25 17:53:21 -0400

    Add email, password, profile picture and time keys

--- models/users.js
+++ models/users.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  spots: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Spots' }],
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
-});
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, minlength: 4 },
+    password: { type: String, required: true, minlength: 6 },
+    email: { type: String, required: true },
+    profPic: String,
+    spots: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Spots' }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+  },
+  { timestamps: true }
+);
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.Model('User', userSchema);
