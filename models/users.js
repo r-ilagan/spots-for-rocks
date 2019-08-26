e85e0ed03f25ab297690bd3be895c96013141b8e
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-26 15:35:33 -0400

    Remove required attribute of password and add isVerified key

--- models/users.js
+++ models/users.js
@@ -4,9 +4,10 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, minlength: 4 },
-    password: { type: String, required: true, minlength: 6 },
+    password: { type: String, minlength: 6 },
     email: { type: String, required: true },
     profPic: String,
+    isVerified: false,
     spots: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Spots' }],
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
   },
