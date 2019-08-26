d743c1f990cc783bedc41003e55c4750223e9bce
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-26 15:50:03 -0400

    Clean routes and put responsibility in userController

--- routes/users.js
+++ routes/users.js
@@ -1,73 +1,24 @@
 const express = require('express');
-const passport = require('passport');
-const User = require('../models/users');
+const userController = require('../controller/users');
 
 const router = express.Router();
 
 // Login route
-router.get('/login', (req, res) => {
-  res.render('auth/login');
-});
+router.get('/login', (req, res) => res.render('auth/login'));
 
 // Register route
-router.get('/register', (req, res) => {
-  res.render('auth/register');
-});
+router.get('/register', (req, res) => res.render('auth/register'));
 
 // Create new user
 router.post('/', (req, res) => {
   const { email, username, password, password2 } = req.body;
-  const errors = [];
-
-  if (!email || !username || !password || !password2) {
-    errors.push({ msg: 'Please fill in all fields' });
-  }
-
-  if (password !== password2) {
-    errors.push({ msg: 'Passwords do not match!' });
-  }
-
-  if (password.length < 6) {
-    errors.push({ msg: 'Password should at least be 6 characters' });
-  }
-
-  if (username.length < 4) {
-    errors.push({ msg: 'Username should at least be 4 characters' });
-  }
-
-  if (username.length > 12) {
-    errors.push({ msg: 'Username should cannot be more than 12 characters' });
-  }
-
-  if (errors.length > 0) {
-    res.render('auth/register', {
-      errors,
-      email,
-      username,
-      password
-    });
-  } else {
-    // Validation and registration
-    const user = new User({
-      username,
-      email: email
-    });
-    User.register(user, password)
-      .then(account => {
-        console.log(account);
-        passport.authenticate('local')(req, res, () => {
-          res.redirect('/spots');
-        });
-      })
-      .catch(err => {
-        errors.push({ msg: err.message });
-        res.render('auth/register', {
-          errors,
-          email,
-          username,
-          password
-        });
-      });
-  }
+  const errors = userController.validateCred(
+    email,
+    username,
+    password,
+    password2
+  );
+  userController.createUser(errors, email, username, password, req, res);
 });
+
 module.exports = router;
