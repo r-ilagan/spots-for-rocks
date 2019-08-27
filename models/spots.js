9e3d910f248e6b56489e73d8e9029b911d3a3d81
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-27 16:35:58 -0400

    Add username and id to author key

--- models/spots.js
+++ models/spots.js
@@ -4,7 +4,10 @@ const spotSchema = new mongoose.Schema(
   {
     name: String,
     description: String,
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
+    author: {
+      id: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
+      username: String
+    },
     image: String,
     wasUpdated: Boolean,
     comment: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
