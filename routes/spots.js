aac19fc10d7906f3c7fc4e398c03bba6f280ffa3
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-27 16:36:59 -0400

    Add middlware that checks for ownership and change author definition

--- routes/spots.js
+++ routes/spots.js
@@ -15,7 +15,8 @@ router.get('/new', middleware.isLoggedIn, (req, res) => {
 
 // Create route
 router.post('/', middleware.isLoggedIn, (req, res) => {
-  const author = req.user;
+  // eslint-disable-next-line no-underscore-dangle
+  const author = { id: req.user._id, username: req.user.username };
   const image = req.body.image;
   const place = req.body.place;
   const description = req.body.description;
@@ -34,17 +35,17 @@ router.get('/:id', (req, res) => {
 });
 
 // Edit route
-router.get('/:id/edit', middleware.isLoggedIn, (req, res) => {
+router.get('/:id/edit', middleware.checkSpotOwnership, (req, res) => {
   spotsController.editSpot(res, req.params.id);
 });
 
 // Update route
-router.put('/:id', middleware.isLoggedIn, (req, res) => {
+router.put('/:id', middleware.checkSpotOwnership, (req, res) => {
   spotsController.updateSpot(req, res, req.params.id);
 });
 
 // Delete route
-router.delete('/:id', middleware.isLoggedIn, (req, res) => {
+router.delete('/:id', middleware.checkSpotOwnership, (req, res) => {
   spotsController.deleteSpot(res, req.params.id);
 });
 
