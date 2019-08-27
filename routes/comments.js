99fa3789dee49620f70f6648f5d444c2a13980dc
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-27 17:07:24 -0400

    Add isLoggedIn() and checkCommentOwnership() middleware to all routes

--- routes/comments.js
+++ routes/comments.js
@@ -9,22 +9,26 @@ router.get('/new', middleware.isLoggedIn, (req, res) => {
 });
 
 // Create route
-router.post('/', (req, res) => {
+router.post('/', middleware.isLoggedIn, (req, res) => {
   commentController.createComment(req, res);
 });
 
 // Edit route
-router.get('/:comment_id/edit', (req, res) => {
-  commentController.editComment(req, res);
-});
+router.get(
+  '/:comment_id/edit',
+  middleware.checkCommentOwnership,
+  (req, res) => {
+    commentController.editComment(req, res);
+  }
+);
 
 // Update route
-router.put('/:comment_id', (req, res) => {
+router.put('/:comment_id', middleware.checkCommentOwnership, (req, res) => {
   commentController.updateComment(req, res);
 });
 
 // Delete route
-router.delete('/:comment_id', (req, res) => {
+router.delete('/:comment_id', middleware.checkCommentOwnership, (req, res) => {
   commentController.deleteComment(req, res);
 });
 
