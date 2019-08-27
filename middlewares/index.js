1a260bfebb95e4f3cfeabd5edf7863db6606d30e
Rach Ilagan <ilagan.rach@gmail.com>
2019-08-27 16:39:10 -0400

    Create checkSpotOwnership() middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable consistent-return */
+const Spot = require('../models/spots');
+const Comment = require('../models/comments');
+
 const middleware = {};
 
 middleware.isLoggedIn = (req, res, next) => {
@@ -6,6 +10,25 @@ middleware.isLoggedIn = (req, res, next) => {
   }
   req.flash('error', 'Please, login first before you can create a spot.');
   return res.redirect('/users/login');
+};
+
+middleware.checkSpotOwnership = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    Spot.findById(req.params.id)
+      .then(spot => {
+        if (!spot) {
+          req.flash('error', 'Spot not found!');
+          return res.redirect('back');
+        }
+        // eslint-disable-next-line no-underscore-dangle
+        if (spot.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect('back');
+        }
+      })
+      .catch(err => console.log(err));
+  }
 };
 
 module.exports = middleware;
