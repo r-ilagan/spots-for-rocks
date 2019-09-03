d4aea4689b4bb5d5eb98ec943c205e3717232a6e
Rach Ilagan <ilagan.rach@gmail.com>
2019-09-03 13:20:21 -0400

    Use handleError() to dry up code

--- controller/comments.js
+++ controller/comments.js
@@ -2,11 +2,16 @@ const Spot = require('../models/spots');
 const Comment = require('../models/comments');
 const User = require('../models/users');
 
+function handleError(err, req, res) {
+  req.flash('error', err.message);
+  res.redirect('back');
+}
+
 module.exports.newForm = (req, res) => {
   Spot.findById(req.params.id)
     .exec()
     .then(spot => res.render('comments/new', { spot: spot }))
-    .catch(err => console.log(err.message));
+    .catch(err => handleError(err, req, res));
 };
 
 module.exports.createComment = (req, res) => {
@@ -17,28 +22,19 @@ module.exports.createComment = (req, res) => {
         promise.then(comment => {
           // Add comment to user profile
           user.comments.push(comment);
-          user.save().catch(err => {
-            req.flash('error', err.message);
-            res.redirect('back');
-          });
+          user.save().catch(err => handleError(err, req, res));
           comment.text = req.body.text;
           comment.author = { id: req.user._id, username: req.user.username };
-          comment.save().catch(err => {
-            req.flash('error', err.message);
-            res.redirect('back');
-          });
+          comment.save().catch(err => handleError(err, req, res));
           // Add comment to spot
           spot.comment.push(comment);
           spot
             .save()
             .then(() => res.redirect(`/spots/${req.params.id}`))
-            .catch(err => {
-              req.flash('error', err.message);
-              res.redirect('back');
-            });
+            .catch(err => handleError(err, req, res));
         });
       })
-      .catch(err => console.log(err.message));
+      .catch(err => handleError(err, req, res));
   });
 };
 
@@ -49,11 +45,9 @@ module.exports.editComment = (req, res) => {
         .then(comment => {
           res.render('comments/edit', { spot: spot, comment: comment });
         })
-        .catch(err =>
-          console.log('Editing comment (find comment): ', err.message)
-        )
+        .catch(err => handleError(err, req, res))
     )
-    .catch(err => console.log('Editing comment (find spot): ', err.message));
+    .catch(err => handleError(err, req, res));
 };
 
 module.exports.updateComment = (req, res) => {
@@ -64,11 +58,11 @@ module.exports.updateComment = (req, res) => {
   })
     .exec()
     .then(() => res.redirect(`/spots/${req.params.id}`))
-    .catch(err => console.log('Update comment: ', err.message));
+    .catch(err => handleError(err, req, res));
 };
 
 module.exports.deleteComment = (req, res) => {
   Comment.findByIdAndRemove(req.params.comment_id)
     .then(() => res.redirect(`/spots/${req.params.id}`))
-    .catch(err => console.log('Deleting commment: ', err.message));
+    .catch(err => handleError(err, req, res));
 };
