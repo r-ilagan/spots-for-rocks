acb6ee98a36d7ff0859b6070d5f3ae1006a08aef
Rach Ilagan <ilagan.rach@gmail.com>
2019-09-03 11:50:46 -0400

    Save newly created comments to user profile.

--- controller/comments.js
+++ controller/comments.js
@@ -1,5 +1,6 @@
 const Spot = require('../models/spots');
 const Comment = require('../models/comments');
+const User = require('../models/users');
 
 module.exports.newForm = (req, res) => {
   Spot.findById(req.params.id)
@@ -9,23 +10,36 @@ module.exports.newForm = (req, res) => {
 };
 
 module.exports.createComment = (req, res) => {
-  Spot.findById(req.params.id)
-    .then(spot => {
-      const promise = Comment.create(req.body);
-      promise.then(comment => {
-        comment.text = req.body.text;
-        comment.author = { id: req.user._id, username: req.user.username };
-        comment
-          .save()
-          .catch(err => console.log('Saving comment: ', err.message));
-        spot.comment.push(comment);
-        spot
-          .save()
-          .then(() => res.redirect(`/spots/${req.params.id}`))
-          .catch(err => console.log('Saving spot with comment: ', err.message));
-      });
-    })
-    .catch(err => console.log(err.message));
+  User.findById(req.user._id).then(user => {
+    Spot.findById(req.params.id)
+      .then(spot => {
+        const promise = Comment.create(req.body);
+        promise.then(comment => {
+          // Add comment to user profile
+          user.comments.push(comment);
+          user.save().catch(err => {
+            req.flash('error', err.message);
+            res.redirect('back');
+          });
+          comment.text = req.body.text;
+          comment.author = { id: req.user._id, username: req.user.username };
+          comment.save().catch(err => {
+            req.flash('error', err.message);
+            res.redirect('back');
+          });
+          // Add comment to spot
+          spot.comment.push(comment);
+          spot
+            .save()
+            .then(() => res.redirect(`/spots/${req.params.id}`))
+            .catch(err => {
+              req.flash('error', err.message);
+              res.redirect('back');
+            });
+        });
+      })
+      .catch(err => console.log(err.message));
+  });
 };
 
 module.exports.editComment = (req, res) => {
